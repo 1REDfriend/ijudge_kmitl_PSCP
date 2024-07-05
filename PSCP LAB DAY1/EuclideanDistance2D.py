e87c00acbb06a6cdb5a5e4410240b1a0c470b6c4
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2024-07-05 17:46:28 +0700

    PSCP 25 day 01

---
+++ PSCP LAB DAY1/EuclideanDistance2D.py
@@ -0,0 +1,9 @@
+"""EuclideanDistance2D"""
+def main() :
+    """EuclideanDistance2D"""
+    q1 = float(input())
+    q2 = float(input())
+    p1 = float(input())
+    p2 = float(input())
+    print((((q1 - p1)**2) + ((q2-p2)**2))**0.5)
+main()
