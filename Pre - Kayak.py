b19923df30a9d47baf64b05736a6cd6d3e624307
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2024-07-05 11:07:04 +0700

    Update Pre - Kayak.py

--- Pre - Kayak.py
+++ Pre - Kayak.py
@@ -6,3 +6,17 @@ def main() :
     party_member = 2*num
     kayak_double = num-1
     kayak_normal = 2
+    sums = 0
+
+    member_weight = [int(i) for i in member_weight]
+    member_weight = sorted(member_weight)
+    member_weight.pop()
+    member_weight.pop()
+    print(member_weight)
+
+    for i in range(len(member_weight) // 2) :
+        i *= 2
+        sums += abs(int(member_weight[i]) - int(member_weight[i + 1]))
+    
+    print(sums)
+main()
