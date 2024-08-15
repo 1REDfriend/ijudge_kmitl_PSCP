1998da083320c955f1e6b22e54534d0fcd1257dc
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2024-08-15 13:49:07 +0700

    Update Sequence XXX.py

--- PSCP LAB DAY5/Sequence XXX.py
+++ PSCP LAB DAY5/Sequence XXX.py
@@ -4,9 +4,20 @@ def main() :
     boxWidth = int(input())
     boxCount = int(input())
 
+    if boxWidth <= 1 :
+        print("*"+(" "+"*")*(boxCount-1))
+        return
     print("*"*(boxWidth) + (" "+("*"*boxWidth))*(boxCount-1))
     # boxWidth หาร 2 ไม่เอาเศษ แล้วลบออก 3 เพื่อเอาแค่ส่วนด้านบน
     for i in range((boxWidth-2)//2) :
-        print(f'*{(" "*i)}*{(" "*(((boxWidth)//2)))}*{" "*i}*')
+        # ที่เอา (boxWidth-(i+i+4) คือ ขนาดของ space 2 ข้าง (i) 
+        # ขนาดของ * ที่มีอยู่
+        print(f'*{(" "*i)}*{(" "*(boxWidth-(i+i+4)))}*{" "*i}*',end="")
+        print((" "+(f'*{(" "*i)}*{(" "*(boxWidth-(i+i+4)))}*{" "*i}*'))*(boxCount-1))
+    print(f'*{" "*((boxWidth//2)-1)}*{" "*((boxWidth//2)-1)}*' , end="")
+    print((" "+(f'*{" "*((boxWidth//2)-1)}*{" "*((boxWidth//2)-1)}*'))*(boxCount-1))
+    for i in range(((boxWidth-2)//2)-1,-1,-1) :
+        print(f'*{(" "*i)}*{(" "*(boxWidth-(i+i+4)))}*{" "*i}*' , end="")
+        print((" "+(f'*{(" "*i)}*{(" "*(boxWidth-(i+i+4)))}*{" "*i}*'))*(boxCount-1))
     print("*"*(boxWidth) + (" "+("*"*boxWidth))*(boxCount-1))
 main()
