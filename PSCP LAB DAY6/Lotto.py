63cf9b5b5958e80469138d832e41c3d97528d88f
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2024-08-20 17:10:48 +0700

    Update Lotto.py

--- PSCP LAB DAY6/Lotto.py
+++ PSCP LAB DAY6/Lotto.py
@@ -1,4 +1,12 @@
 """Lotto"""
+def moreorless(num) :
+    """find num if > 999999 or < 000000"""
+    if num > 999999 :
+        num = "000000"
+    elif num < 0 :
+        num = "999999"
+    return str(num).zfill(6)
+
 def main() :
     """Lotto main"""
     win1 = input().zfill(6)
@@ -10,20 +18,24 @@ def main() :
     lotto = input().zfill(6)
 
     result = 0
-    win1_minus = str(int(win1)-1).zfill(6)
-    win1_plus = str(int(win1)+1).zfill(6)
-    if win1_minus == "-000001" :
-        win1_minus = "999999"
+    win1_minus = moreorless(int(win1)-1)
+    win1_plus = moreorless(int(win1)+1)
 
     if lotto == win1 :
         result += 6_000_000
-    if lotto == win1_minus or lotto == win1_plus :
+    if lotto == win1_minus and lotto == win1_plus :
+        result += 200_000
+    elif lotto in (win1_minus , win1_plus) :
         result += 100_000
     if lotto[-2:] == winLast2 :
         result += 2_000
-    if lotto[:3] == winFirst3_1 or lotto[:3] == winFirst3_2 :
+    if lotto[:3] == winFirst3_1 and lotto[:3] == winFirst3_2 :
+        result += 8_000
+    elif lotto[:3] == winFirst3_1 or lotto[:3] == winFirst3_2 :
         result += 4_000
-    if lotto[-3:] == winLast3_1 or lotto[-3:] == winLast3_2 :
+    if lotto[-3:] == winLast3_1 and lotto[-3:] == winLast3_2 :
+        result += 8_000
+    elif lotto[-3:] == winLast3_1 or lotto[-3:] == winLast3_2 :
         result += 4_000
     print(result)
 main()
