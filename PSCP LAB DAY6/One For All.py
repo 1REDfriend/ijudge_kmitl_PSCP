733d476ec824ff503811cbf5fc13942c75e20658
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2024-08-20 18:33:14 +0700

    create one for all.py

--- PSCP LAB DAY6/One For All.py
+++ PSCP LAB DAY6/One For All.py
@@ -2,5 +2,18 @@
 def main() :
     """One For All main"""
     num = int(input())
+    result = ''
     for i in range(1 , num+1) :
-        rankName = input()
+        name = input()
+        if i >= num :
+            result += name
+        elif not i % 2 :
+            result += f'{name}{"-"*i}'
+        elif i % 2 :
+            result += f'{name}{"*"*i}'
+    if num <= 0 :
+        print()
+    else :
+        result += f'_{num}'
+        print(result)
+main()
