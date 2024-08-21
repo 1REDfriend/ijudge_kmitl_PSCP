41382a833533756cd5f17e66cef81b5ad34cb72a
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2024-08-21 21:44:50 +0700

    Update Shorten.py

--- PSCP LAB DAY6/Shorten.py
+++ PSCP LAB DAY6/Shorten.py
@@ -8,21 +8,5 @@ def main() :
     last = -2
     count = 0
 
-    while True :
-        num = int(input())
-        if num != -1 :
-            if last + 1 == num :
-                stack = f'{first}-{num}'
-            else :
-                first = num
-                if stack :
-                    result += stack + ', '
-                    stack = ''
-                elif count > 0 :
-                    result += str(num) + ', '
-            last = num
-        else :
-            break
-        count += 1
-    print(result)
+   
 main()
