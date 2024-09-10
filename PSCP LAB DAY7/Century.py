6a224137a23f9b053b344ca434f71fd48b065686
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2024-09-10 11:50:26 +0700

    s

---
+++ PSCP LAB DAY7/Century.py
@@ -0,0 +1,32 @@
+"""Century"""
+def main() :
+    """Century main """
+    num = int(input())
+    for _ in range(num) :
+        date = input().strip().split(" ")
+        if date[0] == "B.E." :
+            engYear = int(date[1]) - 543
+            engYear += 100
+            century = engYear // 100
+            two_century = str(engYear)[:2]
+            if not engYear % 100 and str(century) in two_century :
+                century -= 1
+            if century <= 0 or century == float('inf'):
+                print("ERROR")
+                continue
+            print(century)
+
+        elif date[0] == "A.D." :
+            engYear = int(date[1]) + 100
+            century = engYear // 100
+            two_century = str(engYear)[:2]
+            if not engYear % 100 and str(century) in two_century  :
+                century -= 1
+            if century <= 0 or century == float('inf'):
+                print("ERROR")
+                continue
+            print(century)
+
+        else :
+            print("ERROR")
+main()
--- PSCP LAB DAY7/Stamps.py
+++ PSCP LAB DAY7/Stamps.py
@@ -2,16 +2,32 @@
 def main() :
     """Stamps main"""
     eatCount = int(input())
-    needSaid = int(input())
-    stamp = int(input())
+    needPaid = int(input())
+    stampAdd = int(input())
     needStamp = int(input())
     discount = int(input())
 
     stampStack = 0
     result = 0
-    discountNow = 0
+    use_stamp = False
 
-    
+    for _ in range(eatCount) :
+        iPaid = int(input())
+        if stampStack >= needStamp and needStamp:
+            pay = iPaid
+            while stampStack >= needStamp :
+                stampStack -= needStamp
+                pay -= discount
+                if pay <= 0 :
+                    break
+            if pay > 0 :
+                result += pay
+            use_stamp = True
+        if iPaid >= needPaid and needPaid:
+            stampStack += (iPaid // needPaid ) * stampAdd
+        if not use_stamp :
+            result += iPaid
+        use_stamp = False
     print(result)
     print(stampStack)
 main()
