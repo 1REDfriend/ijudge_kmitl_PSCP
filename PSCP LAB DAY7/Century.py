d858381b1d0f14d6c45ef2ae24e823adda607dcc
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2024-09-23 17:45:16 +0700

    Update Century.py

--- PSCP LAB DAY7/Century.py
+++ PSCP LAB DAY7/Century.py
@@ -1,32 +1,22 @@
-"""Century"""
-def main() :
-    """Century main """
+"""century"""
+import math
+
+def main():
+    """century main"""
     num = int(input())
-    for _ in range(num) :
-        date = input().strip().split(" ")
-        if date[0].upper() == "B.E." :
-            engYear = int(date[1]) - 543
-            engYear += 100
-            century = engYear // 100
-            two_century = str(engYear)[:2]
-            if not engYear % 100 and str(century) in two_century :
-                century -= 1
-            if century <= 0 or century == float('inf'):
-                print("ERROR")
-                continue
-            print(century)
+    text_list = []
 
-        elif date[0].upper() == "A.D." :
-            engYear = int(date[1]) + 100
-            century = engYear // 100
-            two_century = str(engYear)[:2]
-            if not engYear % 100 and str(century) in two_century  :
-                century -= 1
-            if century <= 0 or century == float('inf'):
-                print("ERROR")
-                continue
-            print(century)
+    for _ in range(num):
+        text = input()
+        if text[:4] == "A.D.":
+            text_list.append(math.ceil(int(text[5:])/100))
+        elif text[:4] == "B.E.":
+            century = int(text[5:]) - 543
+            if century <= 0:
+                text_list.append("ERROR")
+            else:
+                text_list.append(math.ceil(century/100))
 
-        else :
-            print("ERROR")
+    for i in text_list:
+        print(i)
 main()
